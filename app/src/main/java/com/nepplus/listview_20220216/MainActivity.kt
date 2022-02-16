d1ce153e411757f20fd5c19bbedcc2b0e082d4aa
nepp.plus <nepp.plus@gmail.com>
2022-02-16 16:42:34 +0900

    ItemLongClick 이벤트 별도 처리

--- app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/nepplus/listview_20220216/MainActivity.kt
@@ -39,5 +39,15 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        studentListView.setOnItemLongClickListener { adapterView, view, position, l ->
+
+            val longClickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${longClickedStudent.name} 학생이 길게 클릭됨.", Toast.LENGTH_SHORT)
+                .show()
+
+            return@setOnItemLongClickListener true
+        }
+
     }
 }
